7e481c094c790ea5f8a7f7256cb94ab5eeadb198
Alexandre <alexandrechabeau.pro@gmail.com>
2018-10-20 16:46:25 +0200

    Responsive

--- js/parallax.js
+++ js/parallax.js
@@ -1,6 +1,6 @@
 const background = document.querySelector("#landing");
 const page = document.querySelector("#master");
-let gravity = 0.75;
+let gravity = 0.35;
 const pageWidth = window.innerWidth / gravity;
 const pageHeight = window.innerHeight / gravity;
 
